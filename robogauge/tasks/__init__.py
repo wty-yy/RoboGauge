f55e49e6821d6d04528d7cb7bc81f0c027c0dfc1
wty-yy <993660140@qq.com>
2025-12-05 17:59:22 +0800

    v0.1.7

--- robogauge/tasks/__init__.py
+++ robogauge/tasks/__init__.py
@@ -1,10 +1,11 @@
 from robogauge.utils.task_register import task_register
 from robogauge.tasks.simulator.mujoco_config import MujocoConfig
-from robogauge.tasks.robots import RobotConfig, Go2Config
+from robogauge.tasks.robots import RobotConfig, Go2Config, Go2MoEConfig
 from robogauge.tasks.pipeline import BasePipeline
 from robogauge.tasks.gauge import BaseGaugeConfig
 
-from robogauge.tasks.custom.go2_flat_task import Go2FlatGaugeConfig
+from robogauge.tasks.custom.go2_flat_task import Go2FlatGaugeConfig, Go2FlatConfig, Go2MoEFlatConfig
 
 task_register.register('base', BasePipeline, MujocoConfig, BaseGaugeConfig, RobotConfig)
-task_register.register('go2_flat', BasePipeline, MujocoConfig, Go2FlatGaugeConfig, Go2Config)
+task_register.register('go2_flat', BasePipeline, MujocoConfig, Go2FlatGaugeConfig, Go2FlatConfig)
+task_register.register('go2_moe_flat', BasePipeline, MujocoConfig, Go2FlatGaugeConfig, Go2MoEFlatConfig)
---
+++ robogauge/tasks/robots/go2/go2_moe.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+@File    : go2_moe.py
+@Time    : 2025/12/05 17:29:16
+@Author  : wty-yy
+@Version : 1.0
+@Blog    : https://wty-yy.github.io/
+@Desc    : None
+'''
+import torch
+import numpy as np
+
+from robogauge.tasks.robots.base_robot import BaseRobot, get_projected_gravity
+from robogauge.tasks.robots.go2.go2_config import Go2Config
+from robogauge.tasks.robots.go2.go2 import Go2
+from robogauge.tasks.simulator.sim_data import SimData
+from robogauge.tasks.gauge.goal_data import GoalData
+from robogauge.utils.logger import logger
+
+class Go2MoE(Go2):
+    def get_action(self, obs: np.ndarray):
+        obs_tensor = torch.tensor(obs, dtype=torch.float32).unsqueeze(0).to(self.device)
+        action, weights = self.model(obs_tensor)
+        action = action.detach().cpu().numpy().squeeze(0)[self.model2mj_idx]
+        weights = weights.detach().cpu().numpy().squeeze(0)
+        self.last_action = action
+        target_dof_pos = action * self.action_scale + self.default_dof_pos
+        return target_dof_pos, self.p_gains, self.d_gains, self.control_type
